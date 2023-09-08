b9d07e902ede2c14bc1cf879f51922b69d7ae93f
Matheo <mflorez3@live.com>
2023-09-08 16:58:19 -0400

    author name and download enabled for each image

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MyModal from './Overlay';
 import { useState } from 'react';
+import { BsFillArrowDownSquareFill } from 'react-icons/bs';
 
 const ImageList = ({ images }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,21 +13,33 @@ const ImageList = ({ images }) => {
 
   return (
     <div className="flex mx-6 my-8">
-      <div className="columns-1 md:columns-3 lg:columns-6 md:transition-all md:ease-in-out gap-x-8  space-y-8">
+      <div className="columns-1 md:columns-3 lg:columns-5 md:transition-all md:ease-in-out gap-x-8  space-y-8">
         {images &&
           images.map((image) => {
             return (
-              <img
-                className="pointer rounded shadow-xl hover:scale-105 transition-all "
-                onClick={() => {
-                  setIsOpen(true);
-                  setSelectedImg(image);
-                }}
-                // onClick={() => onClickFullImage(image.urls.full)}
-                key={image.id}
-                alt={image.alt_description}
-                src={image.urls.regular}
-              />
+              <div key={image.id} className="relative">
+                <img
+                  className="pointer rounded shadow-xl hover:scale-105 transition-all hover:contrast-50"
+                  onClick={() => {
+                    setIsOpen(true);
+                    setSelectedImg(image);
+                  }}
+                  alt={image.alt_description}
+                  src={image.urls.regular}
+                />
+                <p className="absolute bottom-4 left-2 h-2 w-auto font-normal text-white text-sm">
+                  {image.user.name}
+                </p>
+                <a
+                  rel="noreferrer"
+                  href={image.urls.full}
+                  target="_blank"
+                  download={image.alt_description}
+                  className=" absolute bottom-4 right-2 h-3 w-auto font-normal text-white text-lg"
+                >
+                  <BsFillArrowDownSquareFill />
+                </a>
+              </div>
             );
           })}
       </div>
